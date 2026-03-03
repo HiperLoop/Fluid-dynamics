2f4eaf2ceee4b2c2129e1129a4533fc385846db1
Adam Zich <adam.zich3@gmail.com>
2026-03-03 10:10:43 +0100

    added universal complex potential function to get complex velocity potential and stream function

--- python/project 1/main.py
+++ python/project 1/main.py
@@ -27,6 +27,13 @@ def velocity_potential_function(x, y, x_vel, y_vel, a, gamma):
     value = U * r * np.cos(theta) * (1 + a**2 / r**2) + gamma / (2 * np.pi) * theta
     return value.real
 
+def complex_potential(x, y, x_vel, y_vel, a, gamma):
+    z = x + 1j * y
+    U = x_vel - 1j * y_vel
+    U_bar = x_vel + 1j * y_vel
+    f = U*z + U_bar * a**2 / z - 1j * gamma / (2 * np.pi) * np.log(z)
+    return f.real, f.imag
+
 def show_plot(x, y):
     fig, ax = plt.subplots()
     for i in range(len(x)):
@@ -36,7 +43,7 @@ def show_plot(x, y):
 
 def plotter(f, c):
     theta = np.linspace(0, 2 * np.pi, 400)
-    r = np.linspace(a, 3, 400)
+    r = np.linspace(a, 3.5, 400)
     #x = np.linspace(-2, 2, 100)
     #y = np.linspace(-2, 2, 100)
     x = z0[0] + r * np.cos(theta)
@@ -44,22 +51,24 @@ def plotter(f, c):
     R, Theta = np.meshgrid(r,theta)
     X = z0[0] + R * np.cos(Theta)
     Y = z0[1] + R * np.sin(Theta)
+    Xj, Yj = joukowski_transform(X, Y, 1)
     Z = f(X,Y)
     x, y = generate_cylinder(a, z0, n=1000)
+    xj, yj = joukowski_transform(x, y, 1)
     fig, ax = plt.subplots()
     #ax.scatter(X, Y, 2)
-    cs = ax.contour(X,Y,Z, levels=40)
-    ax.set_xlim(-2 + z0[0], 2 + z0[0])
-    ax.set_ylim(-2 + z0[1], 2 + z0[1])
-    ax.scatter(x, y, 2)
+    cs = ax.contour(Xj,Yj,Z, levels=100)
+    ax.set_xlim(-2.5 + z0[0], 2.5 + z0[0])
+    ax.set_ylim(-2.5 + z0[1], 2.5 + z0[1])
+    ax.scatter(xj, yj, 2)
     fig.set_size_inches(6, 6)
     plt.show()
 
 c = 0
 a = 1.12
 z0 = (-0.1, 0.22)
-plotter(lambda x, y: stream_function(x, y, 1, 0, a, -3), c)
-plotter(lambda x, y: velocity_potential_function(x, y, 1, 0, a, -3), c)
+plotter(lambda x, y: complex_potential(x, y, 1, 0, a, -3)[1], c)
+plotter(lambda x, y: complex_potential(x, y, 1, 0, a, -3)[0], c)
 
 x, y = generate_cylinder(a, z0, n=1000)
 xj, yj = joukowski_transform(x, y, 1)
