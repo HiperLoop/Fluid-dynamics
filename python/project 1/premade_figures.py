53ec49ab20739e1f10e8a4ef8104b5c6857ed501
Adam Zich <adam.zich3@gmail.com>
2026-03-06 23:25:06 +0100

    did problem P1/2/e

--- python/project 1/premade_figures.py
+++ python/project 1/premade_figures.py
@@ -10,12 +10,13 @@ z0 = (-0.1, 0.22)
 #endregion
 #region c)
 #plotter(plotter_function(1, 0, a, 0), joukowski=True, draw_shape=True, fig_limit=2.5, contours=100)
-#plotter(plotter_function(1, 0, a, -3), joukowski=True, draw_shape=True, fig_limit=2.5, contours=100)
+plotter(plotter_function(1, 0, a, -3), joukowski=True, draw_shape=True, fig_limit=2.5, contours=100)
 #endregion
 #region d)
 #plotter(plotter_function(1, 0, a, -0.2), joukowski=True, draw_shape=True, fig_limit=0.5, fig_offset=[2, -0.2], contours=200)
 #endregion
 #region e)
+#plotter(plotter_function(1, 0, a, -0.1), joukowski=False, draw_shape=True, fig_limit=2.5, contours=100)
 #plotter(plotter_function(1, 0, a, -2.5, stream=True), joukowski=True, draw_shape=True, fig_limit=0.3, fig_offset=[2.1, -0.2], contours=800)
 #endregion
 #region f)
