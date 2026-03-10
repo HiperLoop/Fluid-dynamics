46e0256b15c41b698e19db6a92c4a0f63d1b5ec5
Adam Zich <adam.zich3@gmail.com>
2026-03-10 19:24:31 +0100

    generated new figures for gamma = -2.8

--- python/project 1/premade_figures.py
+++ python/project 1/premade_figures.py
@@ -4,27 +4,27 @@ a = 1.12
 z0 = -0.1 + 0.22j
 correct_gamma = -2.8
 #region a)
-#question_a(save=True, filename='a_scatter.png')
+question_a(save=True, filename='a_scatter.png')
 #endregion
 #region b)
-#plotter(plotter_function(1, a, 0), joukowski=False, draw_shape=True, fig_limit=2.5, contours=100, save=True, filename='b_streamfunction_gamma0.png')
-#plotter(plotter_function(1, a, -3), joukowski=False, draw_shape=True, fig_limit=2.5, contours=100, save=True, filename='b_streamfunction_gamma-3.png')
+plotter(plotter_function(1, a, 0), joukowski=False, draw_shape=True, fig_limit=2.5, contours=100, save=True, filename='b_streamfunction_gamma0.png')
+plotter(plotter_function(1, a, -3), joukowski=False, draw_shape=True, fig_limit=2.5, contours=100, save=True, filename='b_streamfunction_gamma-3.png')
 #endregion
 #region c)
-#plotter(plotter_function(1, a, 0), joukowski=True, draw_shape=True, fig_limit=2.5, contours=100, save=True, filename='c_streamfunction_gamma0.png')
-#plotter(plotter_function(1, a, -3), joukowski=True, draw_shape=True, fig_limit=2.5, contours=100, save=True, filename='c_streamfunction_gamma-3.png')
+plotter(plotter_function(1, a, 0), joukowski=True, draw_shape=True, fig_limit=2.5, contours=100, save=True, filename='c_streamfunction_gamma0.png')
+plotter(plotter_function(1, a, -3), joukowski=True, draw_shape=True, fig_limit=2.5, contours=100, save=True, filename='c_streamfunction_gamma-3.png')
 #endregion
 #region d)
-#plotter(plotter_function(1, a, correct_gamma), joukowski=True, draw_shape=True, fig_limit=0.3, fig_offset=[2.1, -0.2], contours=400, save=True, filename=f'd_streamfunction_gamma-{correct_gamma}.png')
+plotter(plotter_function(1, a, correct_gamma), joukowski=True, draw_shape=True, fig_limit=0.3, fig_offset=[2.1, -0.2], contours=400, save=True, filename=f'd_streamfunction_gamma-{correct_gamma}.png')
 #endregion
 #region e)
-#plotter(plotter_function(1, a, correct_gamma), joukowski=True, draw_shape=True, fig_limit=2.5, contours=100, save=True, filename=f'e_streamfunction_gamma-{correct_gamma}.png')
-#plotter(plotter_function(1, a, correct_gamma, function='stream'), joukowski=True, draw_shape=True, fig_limit=0.3, fig_offset=[2.1, -0.2], contours=800, save=True, filename=f'e_zoomed_streamfunction_gamma-{correct_gamma}.png')
+plotter(plotter_function(1, a, correct_gamma), joukowski=True, draw_shape=True, fig_limit=2.5, contours=100, save=True, filename=f'e_streamfunction_gamma-{correct_gamma}.png')
+plotter(plotter_function(1, a, correct_gamma, function='stream'), joukowski=True, draw_shape=True, fig_limit=0.3, fig_offset=[2.1, -0.2], contours=800, save=True, filename=f'e_zoomed_streamfunction_gamma-{correct_gamma}.png')
 #endregion
 #region f)
-plotter(plotter_function(1, 0, a, correct_gamma, function='pressure'), joukowski=True, draw_shape=True, fig_limit=2.5, contours=100, colourmap='turbo', fill=True, save=True, filename='f_pressure.png')
-#plotter(plotter_function(1, a, correct_gamma, function='jvelocity'), joukowski=True, draw_shape=True, fig_limit=2.5, contours=100, colourmap='turbo', fill=True, save=True, filename='f_joukowski_velocity.png')
+plotter(plotter_function(1, a, correct_gamma, function='pressure'), joukowski=True, draw_shape=True, fig_limit=2.5, contours=100, colourmap='turbo', fill=True, save=True, filename='f_pressure.png')
+plotter(plotter_function(1, a, correct_gamma, function='jvelocity'), joukowski=True, draw_shape=True, fig_limit=2.5, contours=100, colourmap='turbo', fill=True, save=True, filename='f_joukowski_velocity.png')
 #endregion
 #region g)
-#print_force(1, 0, a=a, gamma=correct_gamma)
+print_force(1, a=a, gamma=correct_gamma)
 #endregion
