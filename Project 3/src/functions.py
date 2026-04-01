9596c5c8b2114acf83c1f3be632939d37881e048
Adam Zich <adam.zich3@gmail.com>
2026-04-01 16:26:06 +0200

    added exception handling to ptimal dt finding

--- Project 3/src/functions.py
+++ Project 3/src/functions.py
@@ -7,7 +7,6 @@ PATH = "Project 3/tex/figures/"
 
 # Physical constants
 g = 9.81 # Gravitational acceleration
-H = 1 # Leyer thickness
 nu = 1 # Kinematic viscosity
 Kappa = 1 # Thermal diffusivity
 Rho_0 = 1 # Reference density
@@ -17,9 +16,7 @@ b = 8/3 # Rayleigh number
 theta_top = 1 # Temperature at H
 theta_bot = 0 # Temperature at 0
 theta_delta = theta_top - theta_bot # Temperature difference
-wave_number = 1 # Wave number
-raileigh_number = g * theta_delta * H**3 / (nu * Kappa) # Rayleigh number
-rrr = raileigh_number * wave_number**2 / (wave_number**2 + np.pi**2)**3 # General rescaled reduced Rayleigh number
+wave_number = np.pi / np.sqrt(2) # Wave number
 
 # Basic functions
 def tau_from_t(t):
@@ -60,21 +57,24 @@ def dt_to_2dt_comparison(initial_pos, dt, t_min, t_max, r):
 
 def find_optimal_dt(initial_pos, dt, max_comp_time, max_error, t_min, t_max, r):
     last_time_exceeded = True
+    errors = []
     while True:
         start_time = time.time()
         trajectory_dt1 = simulate_trajectory(initial_pos, dt, t_min, t_max, r)
         duration = time.time() - start_time
         trajectory_dt2 = simulate_trajectory(initial_pos, 2 * dt, t_min, t_max, r)
         error = mean_squared_error(trajectory_dt1, trajectory_dt2)
-        print(f"Computed trajectory for dt={dt} in {duration:.2f} seconds")
+        errors.append((dt, error))
+        print(f"Computed trajectory for dt={dt} in {duration:.2f} seconds with Mean Squared Error: {error}")
         if duration > max_comp_time:
             dt *= duration / max_comp_time # Increase dt to reduce computation time
             if not last_time_exceeded:
                 if error > max_error:
-                    print("No suitable dt found within the maximum computation time and error constraints.")
-                    return None
-                print(f"Optimal dt: {dt} with Mean Squared Error: {error}")
-                return dt
+                    raise Exception("No suitable dt found within the maximum computation time and error constraints.")
+                # Get dt with smallest error that meets the constraints
+                optimal_dt = min((d for d, e in errors if e <= max_error), default=None)
+                print(f"Optimal dt: {optimal_dt} with Mean Squared Error: {min((e for d, e in errors if e <= max_error), default=None)}")
+                return optimal_dt
             last_time_exceeded = True
             continue # Skip this dt if it exceeds the maximum computation time
         last_time_exceeded = False
@@ -93,7 +93,11 @@ def plot_trajectory(positions, save_fig=False, title='Trajectory in Phase Space'
         plt.savefig(PATH + title.replace(" ", "_") + '.png')
     plt.show()
 
-def plot_optimal_trajectory(initial_pos, initial_dt, max_computation_time, max_error, t_min, t_max, r, save_fig=False):
-    dt = find_optimal_dt(initial_pos, initial_dt, max_computation_time, max_error, t_min, t_max, r)
+def plot_optimised_trajectory(initial_pos, initial_dt, max_computation_time, max_error, t_min, t_max, r, H, save_fig=False):
+    try:
+        dt = find_optimal_dt(initial_pos, initial_dt, max_computation_time, max_error, t_min, t_max, r)
+    except Exception as e:
+        print(f"Error finding optimal dt: {e}")
+        return
     trajectory = simulate_trajectory(initial_pos, dt, t_min, t_max, r)
-    plot_trajectory(trajectory, save_fig, title=f"Optimal Trajectory with dt={dt:.4f}")
+    plot_trajectory(trajectory, save_fig, title=f"Trajectory with initial position= {initial_pos} and dt={dt:.7f}")
