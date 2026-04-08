f982602f9018914c7de41e47e149d03f8b74163c
Adam Zich <adam.zich3@gmail.com>
2026-04-08 12:28:26 +0200

    more commenting

--- Project 3/src/functions.py
+++ Project 3/src/functions.py
@@ -18,14 +18,16 @@ theta_bot = 0 # Temperature at 0
 theta_delta = theta_top - theta_bot # Temperature difference
 wave_number = np.pi / np.sqrt(2) # Wave number
 
-# Basic functions
+# ---Basic functions---
+# Functions to convert between t and tau
 def tau_from_t(t):
     return t * (wave_number**2 + np.pi**2)
 
 def t_from_tau(tau):
     return tau / (wave_number**2 + np.pi**2)
 
-# Euler forward method
+# ---Euler forward method---
+# Calculating the derivatives of the position in phase space
 def dpos_dtau(pos, r):
     x, y, z = pos
     dx_dtau = s * (y - x)
@@ -33,9 +35,12 @@ def dpos_dtau(pos, r):
     dz_dtau = x * y - b * z
     return np.array([dx_dtau, dy_dtau, dz_dtau])
 
+# Euler forward method to calculate the next position in phase space
 def euler_forward(pos, dtau, r):
     return pos + dpos_dtau(pos, r) * dtau
 
+# ---Trajectory simulation---
+# Simulate the trajectory in phase space for given initial position, time step, time range, and rescaled reduced Rayleigh number
 def simulate_trajectory(initial_pos, dt, t_min, t_max, r):
     num_steps = int((t_max - t_min) / dt) # Number of time steps
     dtau = tau_from_t(dt);
@@ -45,27 +50,25 @@ def simulate_trajectory(initial_pos, dt, t_min, t_max, r):
         positions[i] = euler_forward(positions[i-1], dtau, r)
     return positions
 
-# Determining dt
+# ---Determining dt---
+# Function to calculate the mean squared error between two trajectories
 def mean_squared_error(positions_dt1, positions_dt2):
     return np.mean((positions_dt1[:-1:2] - positions_dt2)**2)
 
-def dt_to_2dt_comparison(initial_pos, dt, t_min, t_max, r):
-    trajectory_dt1 = simulate_trajectory(initial_pos, dt, t_min, t_max, r)
-    trajectory_dt2 = simulate_trajectory(initial_pos, 2 * dt, t_min, t_max, r)
-    error = mean_squared_error(trajectory_dt1, trajectory_dt2)
-    print(f"Mean Squared Error between dt={dt} and dt={2 * dt}: {error}")
-
+# Function to find the lowest possible dt that meets the maximum computation time and error constraints
 def find_optimal_dt(initial_pos, dt, max_comp_time, max_error, t_min, t_max, r):
     last_time_exceeded = True
     errors = []
     while True:
         start_time = time.time()
+        # Simulate trajectories for dt and 2*dt to calculate the error
         trajectory_dt1 = simulate_trajectory(initial_pos, dt, t_min, t_max, r)
         duration = time.time() - start_time
         trajectory_dt2 = simulate_trajectory(initial_pos, 2 * dt, t_min, t_max, r)
         error = mean_squared_error(trajectory_dt1, trajectory_dt2)
         errors.append((dt, error))
         print(f"Computed trajectory for dt={dt} in {duration:.2f} seconds with Mean Squared Error: {error}")
+        # Adjust dt based on computation time and error
         if duration > max_comp_time:
             dt *= duration / max_comp_time # Increase dt to reduce computation time
             if not last_time_exceeded:
@@ -93,6 +96,7 @@ def plot_trajectory(positions, save_fig=False, title='Trajectory in Phase Space'
         plt.savefig(PATH + title.replace(" ", "_") + '.png')
     plt.show()
 
+# Function to simulate trajectory with optimised dt and plot it, with error handling for cases where no suitable dt is found
 def plot_optimised_trajectory(initial_pos, initial_dt, max_computation_time, max_error, t_min, t_max, r, save_fig=False, title=None, use_initial_dt=False):
     # Error handling if no suitable dt is found
     if not use_initial_dt:
